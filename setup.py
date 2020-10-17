5a812b82a4db33db269a84b610ee8b1b01937648
Francesco Urbani <francescourbanidue@gmail.com>
2020-10-17 12:47:06 +0200

    updated version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='em_waves',
-    version='0.0.1',
+    version='0.0.2',
     author='Francesco Urbani',
     author_email='francescourbanidue@gmail.com',
     packages=['em_waves'],
